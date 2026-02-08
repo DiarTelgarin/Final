b0a3b9e8a611267ab6e46efa061711693e0e000b
Diar <diar2016@list.ru>
2026-02-08 21:26:36 +0500

    localhost changes

--- server.js
+++ server.js
@@ -673,4 +673,4 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`http://localhost:\${PORT}`));
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
